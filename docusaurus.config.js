b531990b53733f1fa2ec76d932ba842899fb6be0
Patel Jaivik <jaivik.patel@fabaf.in>
2024-06-11 15:19:39 +0530

    update deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,6 +11,7 @@ const config = {
   organizationName: "Fab-AF", // Your GitHub organization/user name.
   projectName: "docusaurus", // Your GitHub repo name.
   trailingSlash: false,
+  deploymentBranch: "gh-pages",
 
   presets: [
     [
